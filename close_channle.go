ce5654397654c7de9cc763f4eb16a123d092041a
tao <1216707759@qq.com>
2020-01-02 21:20:59 +0800

    关闭channle

---
+++ close_channle.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+/**
+向被关闭的通道发送数据将会触发panic
+从已关闭的通道接收数据时不会发生阻塞
+*/
+func main() {
+	//创建 具有缓冲的通道
+	ch := make(chan int, 2)
+
+	//向通道发送数据
+	ch <- 22
+	ch <- 88
+
+	//关闭缓冲通道
+	close(ch)
+
+	//遍历缓冲中的数据
+	for i := 0; i < cap(ch)+1; i++ {
+		//从通道中读出
+		value, ok := <-ch
+		fmt.Println(value, ok)
+	}
+}
+
+/**结果
+22 true
+88 true
+0 false 此时通道中已经没有数据，所以获取失败，并且没有发生阻塞
+*/
