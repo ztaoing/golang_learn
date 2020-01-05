7a53d5e8013e41f6794091f4f271b6c79600b653
tao <1216707759@qq.com>
2020-01-05 19:11:06 +0800

    考点:channel 往已经关闭的channel写入数据会panic的

---
+++ closeChannel3.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int, 1000)
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+	}()
+
+	go func() {
+		for {
+			a, ok := <-ch
+			if !ok {
+				fmt.Println("channel has been closed!")
+				return
+			}
+			fmt.Println("a:", a)
+		}
+	}()
+
+	//panic: send on closed channel
+	close(ch)
+	fmt.Println("ok")
+
+	time.Sleep(10 * time.Second)
+}
+
+/**
+考点:channel
+往已经关闭的channel写入数据会panic的
+*/
