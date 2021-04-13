02ed4095e6b07bbf6734436e065ba9ff9b712610
tao <1216707759@qq.com>
2021-04-13 15:34:11 +0800

    add 算法题

--- a/jump55.go
+++ a/jump55.go
@@ -38,6 +38,12 @@ func max(x, y int) int {
 	}
 	return y
 }
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
 
 func canJump2(nums []int) bool {
 	l := len(nums) - 1
@@ -900,8 +906,10 @@ func longestPalindromeSubseq(s string) int {
 	//如果str的长度是5，即i=4->j=5;i=3->j=4,j=5;i=2->j=3,j=4,j=5;i=1->j=2,j=3,j=4,j=5
 	//dp[i][j]只和 dp[i][j-1]、dp[i-1][j-1]、dp[i][j+1]有关
 	//层数不断减少
+	//从下向上遍历
 	for i := n - 2; i >= 0; i-- {
 		//j的范围不断扩大
+		//从左向右遍历
 		for j := i + 1; j < n; j++ {
 			//状态转移
 			if s[i] == s[j] {
@@ -923,7 +931,6 @@ func longestPalindromeSubseq(s string) int {
 //dp[j-1]原来是dp[i][j-1]:是内层for循环上一次算出来的值
 //dp[i+1][j]也是内层for循环计算出来的值
 
-//5题：回文串:面试中常见
 func longestPalindromeSubseq1(s string) int {
 	n := len(s)
 	if n == 0 {
@@ -948,3 +955,324 @@ func longestPalindromeSubseq1(s string) int {
 	}
 	return dp[n-1]
 }
+
+//5题：回文串:面试中常见
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+
+func longestPalindromes(s string) string {
+	start, end := 0, 0
+	for i := range s {
+		//从单节点扩大范围
+		l, r := expand(s, i, i)
+		if end-start < r-l {
+			start, end = l, r
+		}
+		//从偶数节点扩大范围
+		l, r = expand(s, i, i+1)
+		//偶数节点的扩大的范围>奇数节点扩大的范围
+		if end-start < r-l {
+			start, end = l, r
+		}
+	}
+	//返回最大范围的结果集
+	return s[start : end+1]
+}
+
+//以l、r为基础扩大范围
+func expand(s string, l, r int) (int, int) {
+	//相等
+	for l >= 0 && r < len(s) && s[l] == s[r] {
+		//扩大范围
+		l, r = l-1, r+1
+	}
+	//最后一次是不相等的或者越界，将最后一次的扩大范围的操作撤回
+	return l + 1, r - 1
+}
+
+//构造回文串
+//输入一个字符串s，可以在字符串的任意位置插入任意的字符。把s变成回文串，请计算最少要进行多少次的计算
+//回文问题一般都是从字符串的中间向两端扩散，构造回文串也是类似的
+//dp[i][j]：对字符串s[i...j]，最少需要进行dp[i][j]次操作成为回文串
+//当s[i]=s[j]时，不需要要进行任何操作，
+//状态转移就是从小规模的问题的答案，推导出更大规模问题的答案，从base case向其他状态推导
+func minInsert(s string) int {
+	n := len(s)
+	if n == 0 {
+		return 0
+	}
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+		dp[i][i] = 0
+	}
+	//从下向上遍历
+	for i := n - 2; i >= 0; i-- {
+		//从左向右遍历
+		for j := i + 1; j < i; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1]
+			} else {
+				dp[i][j] = min(dp[i][j-1], dp[i+1][j]) + 1
+			}
+		}
+	}
+	return dp[0][n-1]
+}
+
+//压缩成一维数组
+func minInsertP(s string) int {
+	n := len(s)
+	if n == 0 {
+		return 0
+	}
+	dp := make([]int, n)
+	for i := n - 2; i >= 0; i-- {
+		pre := 0
+		for j := i + 1; j < n; j++ {
+			temp := dp[j]
+			if s[i] == s[j] {
+				dp[j] = pre
+			} else {
+				dp[j] = min(dp[j], dp[j-1]) + 1
+			}
+			pre = temp
+		}
+
+	}
+	return dp[n-1]
+}
+
+//TODO 题10：正则表达式
+
+//TODO  887题：高楼扔鸡蛋,在最坏情况下，最少要扔几次鸡蛋（），才能确定这个楼层？
+//国内的大厂以及谷歌等都经常面试这道题
+//还有一种极其高效的解法
+
+//TODO 312题:戳气球
+
+/**
+背包问题
+*/
+//0-1背包：给你一个可装载重量为W的背包和N个物品，每个物品有重量和价值两个属性。其中第i个物品的重量为wt[i],价值为val[i],现在用这个背包装物品，最多能装的价值是多少？
+//状态：背包的容量和可选择的物品
+//选择：装和不装
+// dp[i][w]：对于前i个物品，当前背包的容量为w,这种情况下可以装的最大价值(这是背包问题的典型套路)
+// 没有物品或者没有空间的时候，能装的价值为0，即dp[0][...] dp[...][0]
+
+func packet1(w, n int, wt []int, val []int) int {
+	dp := make([][]int, len(wt))
+	for i := 0; i < len(wt); i++ {
+		dp[i] = make([]int, len(wt))
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 1; j <= w; w++ {
+			if j-wt[i-1] < 0 {
+				//背包不够了，只能不装
+				dp[i][w] = dp[i-1][w]
+			} else {
+				dp[i][w] = max(dp[i-1][w], dp[i-1][j-wt[i]]+val[i])
+			}
+		}
+	}
+	return dp[n][w]
+}
+
+//子背包
+//416题：给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+// 解读：给一个可以装在sum/2的背包和n个物品，每个物品的重量为nums[i],现在让你装物品，是否存在一种装法，恰好装满容量为sum/2的背包
+func canPartition(nums []int) bool {
+	n := len(nums)
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	//如果个数为奇数，不能划分为相等的集合
+	if n%2 != 0 {
+		return false
+	}
+	sum = sum / 2
+	dp := make([][]bool, n+1)
+	//base case
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]bool, sum+1)
+	}
+	for i := 0; i < n; i++ {
+		dp[i][0] = true
+	}
+	for i := 1; i < n; i++ {
+		for j := 1; j < sum; j++ {
+			if j-nums[i-1] < 0 {
+				//不能装了
+				dp[i][j] = dp[i-1][j]
+			} else {
+				//装或不装
+				dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]]
+			}
+		}
+	}
+	return dp[n][sum]
+}
+func canPartition1(nums []int) bool {
+	totalNum := 0
+	for index := range nums {
+		totalNum += nums[index]
+	}
+	if totalNum%2 != 0 {
+		return false
+	}
+	numCount := len(nums)
+
+	row := numCount
+	col := totalNum / 2
+	dp := make([][]bool, row+1)
+	for i := 0; i <= row; i++ {
+		dp[i] = make([]bool, col+1)
+	}
+	for i := 0; i <= row; i++ {
+		dp[i][0] = true
+	}
+	for i := 1; i <= row; i++ {
+		for w := 1; w <= col; w++ {
+			if w-nums[i-1] < 0 {
+				dp[i][w] = dp[i-1][w]
+			} else {
+				//dp[i][j]代表 前i个数中集合结果等于j的情况是true/false。
+				dp[i][w] = dp[i-1][w] || dp[i-1][w-nums[i-1]]
+			}
+
+		}
+	}
+	return dp[row][col]
+}
+
+//进行状态压缩
+///进行状态压缩
+//dp[i][j] 都是通过上一行 dp[i-1][..] 转移过来的
+func canPartition2(nums []int) bool {
+	totalNum := 0
+	for index := range nums {
+		totalNum += nums[index]
+	}
+	if totalNum%2 != 0 {
+		return false
+	}
+	numCount := len(nums)
+
+	row := numCount
+	col := totalNum / 2
+	dp := make([]bool, col+1)
+	dp[0] = true
+	for i := 1; i <= row; i++ {
+		for w := col; w > 0; w-- {
+			if w-nums[i-1] >= 0 {
+				dp[w] = dp[w] || dp[w-nums[i-1]]
+			}
+		}
+	}
+	return dp[col]
+}
+
+//完全背包
+//518题：零钱兑换2
+//给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
+
+func change(amount int, coins []int) int {
+	n := len(coins)
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, amount+1)
+	}
+	for i := 0; i <= n; i++ {
+		dp[i][0] = 1
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= amount; j++ {
+			if j-coins[i-1] >= 0 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+	return dp[n][amount]
+}
+
+//由于dp[i]只和dp[i-1]有关，可以进行状态压缩
+func change2(amount int, coins []int) int {
+	n := len(coins)
+	dp := make([]int, amount+1)
+	dp[0] = 1
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= amount; j++ {
+			if j-coins[i-1] >= 0 {
+				dp[j] = dp[j] + dp[j-coins[i-1]]
+			}
+		}
+	}
+	return dp[amount]
+}
+
+//TODO 线性排列
+//198题： 打家劫舍
+
+//TODO 环形对排列
+//213题：
+
+//TODO 树型排列
+//337题：
+
+//全排列
+//46. 全排列
+
+//47. 全排列 II
+//给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+
+//51. N 皇后
+//52. N皇后 II
+//111. 二叉树的最小深度
+
+//双指针
+
+//141. 环形链表
+//给定一个链表，判断链表中是否有环。
+
+//142. 环形链表 II
+
+//已知链表中有环，返回这个环的起始位置
+
+//寻找无环链表的重点
+
+//寻找单链表的倒数第K个元素
+
+//707题。设计链表
+
+//二分搜索：寻找左侧边界的二分搜索
+
+//二分搜索：寻找右侧边界的二分搜索
+
+//滑动窗口：最小覆盖子串
+
+//字符串排列
+
+//找所有字母异位词
+
+//最长无重复子串
+
+/**
+树的遍历
+*/
+
+//二叉树的遍历
+
+//54. 螺旋矩阵
+
+//650. 只有两个键的键盘
+
+//24点游戏（679）
+
+//灯泡开关（319）
