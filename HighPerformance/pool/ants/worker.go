ba24d89642bb10e9d7c6370fe476d0f64864f04e
tao <1216707759@qq.com>
2021-04-19 15:56:18 +0800

    goWorker 是实际的执行任务的人，它使用一个goroutine接收任务，然后使用指定的方法处理这个任务

--- HighPerformance/pool/ants/worker.go
+++ HighPerformance/pool/ants/worker.go
@@ -27,14 +27,12 @@ import (
 	"time"
 )
 
-// goWorker is the actual executor who runs the tasks,
-// it starts a goroutine that accepts tasks and
-// performs function calls.
+// goWorker 是实际的执行任务的人，它使用一个goroutine接收任务，然后使用指定的方法处理这个任务
 type goWorker struct {
-	// pool who owns this worker.
+	// 拥有当前worker的指针
 	pool *Pool
 
-	// task is a job should be done.
+	// 需要被执行的任务
 	task chan func()
 
 	// recycleTime will be update when putting a worker back into queue.
@@ -43,23 +41,32 @@ type goWorker struct {
 
 // run starts a goroutine to repeat the process
 // that performs the function calls.
+// run 开启了一个goroutine执行指定的方法来处理任务
 func (w *goWorker) run() {
+	// 增加运行的goroutine数量
 	w.pool.incRunning()
 	go func() {
+		// 在任务处理完成后，
 		defer func() {
 			w.pool.decRunning()
+			// 将worker归还到workerCache中
 			w.pool.workerCache.Put(w)
+			//处理异常
 			if p := recover(); p != nil {
+				// 使用定制的PanicHandler
 				if ph := w.pool.options.PanicHandler; ph != nil {
 					ph(p)
 				} else {
 					w.pool.options.Logger.Printf("worker exits from a panic: %v\n", p)
 					var buf [4096]byte
+					// 获取此时的运行栈
 					n := runtime.Stack(buf[:], false)
 					w.pool.options.Logger.Printf("worker exits from panic: %s\n", string(buf[:n]))
 				}
 			}
-			// Call Signal() here in case there are goroutines waiting for available workers.
+			// 没有发生panic：
+			// 调用 Signal()通知那些等待获取可用goroutine的被阻塞的调用者
+			// here in case there are goroutines waiting for available workers.
 			w.pool.cond.Signal()
 		}()
 
@@ -67,7 +74,9 @@ func (w *goWorker) run() {
 			if f == nil {
 				return
 			}
+			// 执行每一个任务
 			f()
+			// 执行完，将worker归还到pool中
 			if ok := w.pool.revertWorker(w); !ok {
 				return
 			}
