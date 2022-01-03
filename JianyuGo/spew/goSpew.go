0bc532a179de5aa630318299cbfb0dd802f167e4
tao <1216707759@qq.com>
2022-01-03 10:41:10 +0800

    chan with select

---
+++ JianyuGo/spew/goSpew.go
@@ -0,0 +1,35 @@
+package main
+
+import "github.com/davecgh/go-spew/spew"
+
+type Instance struct {
+	a string
+	b int
+	c *inner
+}
+type inner struct {
+	d string
+	e string
+}
+
+func main() {
+	ins := Instance{
+		a: "aaa",
+		b: 1000,
+		c: &inner{
+			d: "ddd",
+			e: "eee",
+		},
+	}
+	spew.Dump(ins)
+	/**
+	(main.Instance) {
+	 a: (string) (len=3) "aaa",
+	 b: (int) 1000,
+	 c: (*main.inner)(0xc000022100)({
+	  d: (string) (len=3) "ddd",
+	  e: (string) (len=3) "eee"
+	 })
+	}
+	*/
+}
